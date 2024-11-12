36325509fa2e84570c7f5271f48bea47cf9515d4
anmolmangat9 <anmol.mangat@student.kpu.ca>
2024-11-12 17:08:05 +0530

    Delete SortableArrayTest.java

--- SortableArrayTest.java
+++
@@ -1,7 +0,0 @@
-@Test
-void testSortStrings() {
-    SortableArray sortableArray = new SortableArray();
-    String[] input = {"banana", "apple", "cherry"};
-    String[] expected = {"apple", "banana", "cherry"};
-    assertArrayEquals(expected, sortableArray.sortStrings(input));
-}
