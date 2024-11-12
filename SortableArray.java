c2a5f8e6f645e4d216af3fffb3661df9cb787f77
anmolmangat9 <anmol.mangat@student.kpu.ca>
2024-11-12 15:51:16 +0530

    Update SortableArray.java

--- SortableArray.java
+++ SortableArray.java
@@ -26,7 +26,7 @@ public class SortableArray {
 
     public static void main(String[] args) {
         // Test Feature 1: Sort integers
-        int[] integers = {5, 3, 8, 10, 2};
+        int[] integers = {5, 3, 8, 9, 2};
         System.out.println("Original Integers: " + Arrays.toString(integers));
         int[] sortedIntegers = sortIntegers(integers);
         System.out.println("Sorted Integers: " + Arrays.toString(sortedIntegers));
