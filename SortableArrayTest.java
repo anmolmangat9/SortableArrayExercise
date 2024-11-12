02f8c99aedad53a2fbcdd179c6bc70309daed64f
anmolmangat9 <anmol.mangat@student.kpu.ca>
2024-11-12 14:55:29 +0530

    Create SortableArrayTest.java

---
+++ SortableArrayTest.java
@@ -0,0 +1,7 @@
+@Test
+void testSortStrings() {
+    SortableArray sortableArray = new SortableArray();
+    String[] input = {"banana", "apple", "cherry"};
+    String[] expected = {"apple", "banana", "cherry"};
+    assertArrayEquals(expected, sortableArray.sortStrings(input));
+}
