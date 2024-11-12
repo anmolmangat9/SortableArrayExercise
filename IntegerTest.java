4d79d469557b6449f6f61526ad7f38a88f811b30
anmolmangat9 <anmol.mangat@student.kpu.ca>
2024-11-12 16:47:04 +0530

    Update IntegerTest.java

--- IntegerTest.java
+++ IntegerTest.java
@@ -6,10 +6,9 @@ import org.junit.jupiter.api.Test;
 public class SortableArrayTest {
 
     @Test
-    void testSortIntegers() {
-        SortableArray sortableArray = new SortableArray();
-        int[] input = {3, 1, 2};
-        int[] expected = {1, 2, 3};
-        assertArrayEquals(expected, sortableArray.sortIntegers(input));
-    }
-}
+    public static void main(String[] args) {
+        // Test Feature 1: Sort integers
+        int[] integers = {5, 3, 8, 9, 2};
+        System.out.println("Original Integers: " + Arrays.toString(integers));
+        int[] sortedIntegers = sortIntegers(integers);
+        System.out.println("Sorted Integers: " + Arrays.toString(sortedIntegers)}
